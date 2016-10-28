3f6b11b4869c6c1d8ed1cfce3081fb46b491992d
Seth Kendall <sethpkendall@gmail.com>
2016-10-28 09:03:03 -0700

    add genereal answers to index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
       questions: this.store.findAll('question'),
-      answers: this.store.findAll('answers')
+      answers: this.store.findAll('answer')
     });
   },
   actions:{
