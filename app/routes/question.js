a908b9e3a7451c5623827b14fd60aae1a55d5850
Seth Kendall <sethpkendall@gmail.com>
2016-10-28 09:59:48 -0700

    link answers to questions, start new answer form build

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('question', params.question_id);
+    return Ember.RSVP.hash({
+      question: this.store.findRecord('question', params.question_id),
+      answers: this.store.findAll('answer')
+    });
+
   },
   actions: {
     destroyQuestion(question) {
@@ -19,6 +23,14 @@ export default Ember.Route.extend({
       this.set('updateQuestionForm',false);
       this.transitionTo('index');
     },
-
+    saveAnswer(params){
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('questions').addObject(newAnswer);
+      newAnswer.save().then(function(){
+        return question.save();
+      });
+      this.transitionTo('question', question);
+    }
   }
 });
