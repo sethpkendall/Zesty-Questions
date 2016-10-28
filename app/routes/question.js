3e851a25dcb1f44bdbea0b981772699a4498d908
Seth Kendall <sethpkendall@gmail.com>
2016-10-28 10:21:25 -0700

    finish new answer form

--- app/routes/question.js
+++ app/routes/question.js
@@ -26,11 +26,11 @@ export default Ember.Route.extend({
     saveAnswer(params){
       var newAnswer = this.store.createRecord('answer', params);
       var question = params.question;
-      question.get('questions').addObject(newAnswer);
+      question.get('answers').addObject(newAnswer);
       newAnswer.save().then(function(){
         return question.save();
       });
-      this.transitionTo('question', question);
+      this.transitionTo('question');
     }
   }
 });
