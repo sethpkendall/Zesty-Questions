0d1f8f1b8b68d286dcdc1f180fcbd67c66bd2c69
Seth Kendall <sethpkendall@gmail.com>
2016-11-04 11:23:44 -0700

    sort answers by votes

--- app/components/answer-list.js
+++ app/components/answer-list.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['votes:desc'],
+  sortedAnswers: Ember.computed.sort('answers', 'sortBy'),
   actions:{
     voteUp(answer){
       this.sendAction("voteUp", answer)
+    },
+    voteDown(answer){
+      this.sendAction("voteDown", answer)
     }
   }
 });
