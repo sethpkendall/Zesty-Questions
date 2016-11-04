d9b871ffb7dd1509a68d4cd288f27c2b39268884
Seth Kendall <sethpkendall@gmail.com>
2016-11-04 10:28:01 -0700

    add 'no-answers' helper

---
+++ app/helpers/no-answers.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export function noAnswers(params) {
+  var answer = params[0];
+  if (answer.content.length === 0) {
+    return Ember.String.htmlSafe('<span style="color:white"><span {{action class="glyphicon glyphicon-alert"></span> No Answers yet. Click to add one now:</span>');
+  }
+}
+
+export default Ember.Helper.helper(noAnswers);
---
+++ tests/unit/helpers/no-answers-test.js
@@ -0,0 +1,12 @@
+
+import { noAnswers } from 'question-board/helpers/no-answers';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | no answers');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = noAnswers([42]);
+  assert.ok(result);
+});
+
